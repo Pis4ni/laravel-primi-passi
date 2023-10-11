24520d5387123cf33e37f3933b030e99881ed7d7
Pis4ni <pis4wrk@gmail.com>
2023-10-11 15:53:42 +0200

    first commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <main>
+        <div class="wrapper">
+            <div class="container">
+                <div class="text-center vh-100 d-flex align-items-center justify-content-center">
+                    <h1>
+                        Hello World!!
+                    </h1>
+                </div>
+            </div>
+        </div>
+
+    </main>
+</body>
+
+</html>
